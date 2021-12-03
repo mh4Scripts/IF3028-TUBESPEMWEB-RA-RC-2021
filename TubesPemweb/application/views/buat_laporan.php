e58d257a14e265b8b27bc1c86df72885a295e1a1
joyrav09 <56099222+joyrav09@users.noreply.github.com>
2021-12-03 09:15:02 +0700

    Update buat_laporan.php

--- TubesPemweb/application/views/buat_laporan.php
+++ TubesPemweb/application/views/buat_laporan.php
@@ -1,30 +1,60 @@
 <html>
 <head>
-	<title>Tambah Laporan</title>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Tubes Pemweb</title>
+
+	<link rel="stylesheet" type="text/css" href="<?php echo base_url() ?>theme/theme-buat_laporan/style.css">
 </head>
 <body>
-	<center>
-		<h3>Tambah data baru</h3>
-	</center>
-	<form action="<?php echo base_url().'dashboard/tambah_aksi'; ?>" method="post">
-		<table style="margin:20px auto;">
-			<tr>
-				<td>Judul</td>
-				<td><input type="text" name="judul"></td>
-			</tr>
-			<tr>
-				<td>Isi</td>
-				<td><input type="text" name="isi"></td>
-			</tr>
-			<tr>
-				<td>Aksi</td>
-				<td><input type="text" name="aspek"></td>
-			</tr>
-			<tr>
-				<td></td>
-				<td><input type="submit" value="Tambah"></td>
-			</tr>
-		</table>
-	</form>	
+
+	<div id="box">
+		<div id="box-content">
+			<div id="title">
+				<h1>SIMPLE LAPOR!</h1>
+			</div>
+
+			<div id="info-content">
+				<p>Laporan/Komentar Terakhir</p>
+				<hr>
+			</div>
+
+			<div id="form-insert">
+				
+				<?php echo form_open_multipart('dashboard/tambah_aksi'); ?>
+					<table style="margin:20px auto;">
+						<tr>
+							<td><textarea style="padding:15px" cols="110" rows="15" type="text" name="isi" placeholder="Laporan/Komentar"></textarea></td>
+						</tr>
+						<tr>
+							<td><br>
+								<select name="aspek">
+								   <option value="None">Pilih Aspek Pelaporan/Komentar</option>
+								   <option value="A">A</option>
+								   <option value="B">B</option>
+								   <option value="C">C</option>
+								   <option value="D">D</option>
+								</select>
+							</td>
+						</tr>
+
+						<tr>
+							<td><br>
+								<div id="choose-file" name="input-file">
+									<input type="file" name="file" value="choose-file" required="">
+								</div>
+							</td>
+						</tr>
+						<tr>
+							<td><br><input type="submit" value="Tambah"></td>
+						</tr>
+					</table>
+				
+				<?php echo form_close(); ?>
+			</div>	
+		</div>
+	</div>
+	
+
 </body>
 </html>
