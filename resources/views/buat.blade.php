2b89e5c1f1850d05a18f6d0368e5109a5d1195c7
Laras310 <nurullarasati310@gmail.com>
2021-12-05 12:13:26 +0700

    tambah laravel

---
+++ resources/views/buat.blade.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <link rel="stylesheet" href="buat.css">
+        <title>Lapor</title>
+    </head>
+
+    <home>
+        <div class="container">
+            <header>
+                <h1 class="title">SIMPLE LAPOR!</h1>
+            </header>
+            Buat Laporan/Komentar
+            <hr>
+            <br>
+            <div class="laporan">
+                Laporan/komentar terakhir<br>
+                <hr>
+                <p>I inadvertently went to See's Candy last week (I was in the mall looking for phone repair), and as it turns out, See's Candy now charges a dollar -- a full dollar -- for even the simplest of their wee confection offerings. I bought two chocolate lollipops and two chocolate-caramel-almond things. The total cost was four-something. I mean, the candies were tasty and all, but let's be real: A Snickers bar is fifty cents. After this dollar-per-candy revelation, I may not find myself wandering dreamily back into a See's Candy any time soon.</p>
+                <div class="lampiran">
+                    lampiran: gambar.jpg
+                </div>
+                <div class="selengkapnya">
+                    Lihat selengkapnya
+                </div>
+                <div class="waktu">
+                    Waktu: 04-01-2021
+                </div>
+                
+                <br><br><hr>
+                
+            </div>
+
+
+        </div>
+    </home>
+</html>
