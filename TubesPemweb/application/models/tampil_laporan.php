05ac238c9eff657b93cef52d1b90a879a14c70ac
joyrav09 <56099222+joyrav09@users.noreply.github.com>
2021-12-06 14:53:06 +0700

    Update tampil_laporan.php

--- TubesPemweb/application/models/tampil_laporan.php
+++ TubesPemweb/application/models/tampil_laporan.php
@@ -8,5 +8,17 @@ class Tampil_laporan extends CI_Model{
 	public function input_data($data){
 		return $this->db->insert('tb_laporan', $data);
 	}
+
+	public function tampil_artikel($id){
+		$this->db->where('id', $id);
+		$query = $this->db->get('tb_laporan');
+		return $query->result();
+	}
+
+	public function hapus_laporan($where, $table){
+		$this->db->where($where);
+		$this->db->delete($table);
+	} 
+
 }
 ?>
