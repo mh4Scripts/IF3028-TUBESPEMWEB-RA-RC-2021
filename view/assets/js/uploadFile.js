0a76d0dc9b5b177c42a4a45f4c523fd9102df642
SicilliaPutriAisyah-118140091 <sicillia.118140091@student.itera.ac.id>
2021-12-21 16:44:26 +0700

    delete

--- view/assets/js/uploadFile.js
+++
@@ -1,7 +0,0 @@
-const uploadBtn = document.getElementById("upload-btn");
-
-const fileChoosen = document.getElementById("file-choosen");
-
-uploadBtn.addEventListener("change", function () {
-  fileChoosen.textContent = this.files[0].name;
-});
