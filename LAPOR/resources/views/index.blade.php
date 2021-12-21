8296cc15a1dde682d96733e8119abb4a88d159f0
DimasAprilian <dimas.118140032@student.itera.ac.id>
2021-12-21 18:16:28 +0700

    update index blade to show posts

--- LAPOR/resources/views/index.blade.php
+++ LAPOR/resources/views/index.blade.php
@@ -4,7 +4,7 @@
 <head>
   <title>SIMPLE LAPOR</title>
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-  <link rel="stylesheet" href="assets/css/style.css"/>
+  <link rel="stylesheet" href="{{ asset('assets') }}/css/style.css"/>
 </head>
 
 <body>
@@ -26,24 +26,20 @@
       <div class="col text-left">Laporan/Komentar Terakhir</div>
     </div>
 
-
+    @foreach ($posts as $post)
     <div class="content">
       <div class="content-body">
-        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's
-        standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make
-        a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting,
-        remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing
-        Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions
-        of Lorem Ipsum.
+        {!! nl2br(e($post->body)) !!}
       </div>
       <div class="row content-footer">
-        <div class="col text-left">Lampiran : Gambar.jpg</div>
+        <div class="col text-left">Lampiran : {{ $post->attachment ?? "-"}}</div>
         <div class="col text-right">
-          <span>Waktu 20-11-2019 20:00</span>
-          <a href="#">Lihat Selengkapnya <i class="fa fa-angle-right"></i></span>
+          <span>Waktu : {{ $post->created_at }}</span>
+          <a href="/post/{{ $post->id }}">Lihat Selengkapnya <i class="fa fa-angle-right"></i></span>
         </div>
       </div>
     </div>
+    @endforeach
   </div>
 
 </body>
