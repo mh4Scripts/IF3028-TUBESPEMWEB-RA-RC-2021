18c0ea7702a9428dfded3e9d38f750d541a07113
nataliasagitamarisol <natalia.119140062@student.itera.ac.id>
2021-12-25 16:35:09 +0700

    Create tentanglapor.blade.php

---
+++ backend/resources/views/tentanglapor.blade.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <title>Tentang Lapor</title>
+    <body background = "bg.jpeg">
+</head>
+
+<body>
+    <header><b>
+            <center>
+                <iframe width="560" height="315" src="https://www.youtube.com/embed/Oi3wLzswoz4" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+            </center>
+        </b></header>
+
+    <article>
+        <p style="text-align: justify;text-indent: 0.5in;">
+            Lapor adalah layanan penyampaian semua aspirasi dan pengaduan masyarakat secara nasional. Lapor telah ditetapkan sebagai Sistem Pengelolaan Pengaduan Pelayanan Publik Nasional (SP4N) berdasarkan Peraturan Presiden Nomor 76 Tahun 2013 dan Peraturan Menteri
+            Pendayagnaan Aparatur Negara dan Reformasi Birokrasi Nomor 3 Tahun 2015.<br><br>Tujuan SP4N sebagai berikut : <br> 1. Penyelenggara dapat mengelola pengaduan dari masyarakat secara sederhana, cepat, tepat,
+            tuntas, dan terkoordinasi dengan baik; <br> 2. Penyelenggara memberikan akses untuk partisipasi masyarakat dalam menyampaikan pengaduan; dan <br> 3. Meningkatkan kualitas pelayanan publik.
+        </p>
+    </article>
+</body>
+
+</html>
