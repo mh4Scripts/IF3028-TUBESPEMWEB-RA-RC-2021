d24eb2658058e995fc0225461ddec326b2caefd8
CandraWahyuF <candrawahyuf@gmail.com>
2021-12-25 18:24:47 +0700

    edit css detail_page

--- resources/views/detail_page.blade.php
+++ resources/views/detail_page.blade.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="detail_page.css">
+    <link rel="stylesheet" href="/detail_page.css">
     <title>
         SIMPLE LAPOR!
     </title>
