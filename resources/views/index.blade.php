3b736d160f42772cbe4a413e20d275233e3ae98b
barbar17 <akbarmaulana644@gmail.com>
2021-12-25 17:50:31 +0700

    installasi frame work laravel dan buat tampilan awal

---
+++ resources/views/index.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Halaman Awal</title>
+</head>
+
+<body>
+    <h1>LAPORIN</h1>
+    <input type="text">
+    <input type="submit" value="Cari">
+    <br>
+    <a href="/buat_laporan">Buat laporan/komentar +</a>
+    <br>
+    Laporan/Komentar Terakhir
+    <hr>
+</body>
+
+</html>
