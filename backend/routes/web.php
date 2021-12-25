4ee18ce331b72d236cd8766171e9dfe8969c2ec6
huslianapratiwi <husliana.119140042@student.itera.ac.id>
2021-12-25 13:33:21 +0700

    Update web.php

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -14,5 +14,9 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('indeks');
+});
+
+Route::get('/search', function () {
+    return view('search');
 });
