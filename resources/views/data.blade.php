7acea59b97320dc8017a23689afedd039e6d1778
andhikapp28 <andhika.119140224@student.itera.ac.id>
2021-12-25 13:24:32 +0700

    update tampilan laporan

--- resources/views/data.blade.php
+++ resources/views/data.blade.php
@@ -1,3 +1,4 @@
+
 <!DOCTYPE html>
 <html>
 <head>
@@ -16,28 +17,26 @@
             <p>Detail Laporan/Komentar </p>
             <hr>
         </div>
-        @foreach($data as $DATA)
             <div class="list-item">
-                <p>{{$DATA->komentar}}</p>
+                <p>Komentar</p>
 
                 <div class="list-lampiran">
                     <p>Lampiran : </p>
 
-                    <img src="/file_baru/{{$DATA->lampiran}}" width="300px" height="300px">
+                    <img src="#" width="300px" height="300px">
                 </div>
 
                 <div class="list-nav">
-                    <span class="aspek">Waktu: {{$DATA->waktu}}   </span>
-                    <span>Aspek: {{$DATA->aspek}}</span>
+                    <span class="aspek">Waktu: Waktu</span>
+                    <span>Aspek: Aspek</span>
                     <span class="nav1">
-                        <a  href="/update/{{$DATA->id}}">Update | </a>
-                        <a  href="/delete/{{$DATA->id}}" onclick="return confirm('Ingin menghapus data ?')">Hapus Laporan/Komentar &#10008;</a>
+                        <a  href="#">Update | </a>
+                        <a  href="#" onclick="return confirm('Ingin menghapus data ?')">Hapus Laporan/Komentar</a>
                     </span>
                 </div>
 
                 <hr>
             </div>
-        @endforeach
     </div>
 </div>
 </body>
