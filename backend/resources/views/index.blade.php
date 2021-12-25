0cce8361654f5f30830dc4090ac798686270be27
nataliasagitamarisol <natalia.119140062@student.itera.ac.id>
2021-12-25 22:44:38 +0700

    Create index.blade.php

---
+++ backend/resources/views/index.blade.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+  <title>Lapor | Home</title>
+  <link rel="stylesheet" type="text/css" href="css/style.css" />
+</head>
+
+<body>
+  <ul>
+    <li><a href="/tentang">Tentang Kami</a></li>
+    <li><a href="/laporan">Laporan</a></li>
+    <li><a href="/search">Cari Aduan</a></li>
+    <li><a href="/kontak">Kontak</a></li>
+  </ul>
+  <center>
+    <h1>Buat Laporan</h1>
+    <center>
+      <form method="POST" action="proses_tambah.php" enctype="multipart/form-data">
+        <section class="base">
+          <div>
+            <label>Laporan/Komentar</label>
+            <input type="textarea" name="nama_produk" autofocus="" required="" />
+          </div>
+          <div>
+            <label>Pilih Aspek Pelaporan/Komentar</label>
+            <select name="deskripsi">
+              <option value="Dosen">Dosen</option>
+              <option value="Staff">Staff</option>
+              <option value="Mahasiswa">Mahasiswa</option>
+              <option value="Infrastruktur">Infrastruktur</option>
+              <option value="Pengajaran">Pengajaran</option>
+            </select>
+          </div>
+          <div>
+            <label>Lampiran</label>
+            <input type="file" name="gambar_produk" required="" />
+          </div>
+          <div>
+            <button type="submit">Lapor!</button>
+          </div>
+        </section>
+      </form>
+</body>
+
+</html>
