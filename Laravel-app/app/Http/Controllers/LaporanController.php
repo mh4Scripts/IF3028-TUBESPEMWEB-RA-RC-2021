ef7ee86385f0fe9eec31d9e4d950381708774db0
meepshere <sayyidumaru12@gmail.com>
2021-12-25 21:34:33 +0700

    fix lampiran controller

--- Laravel-app/app/Http/Controllers/LaporanController.php
+++ Laravel-app/app/Http/Controllers/LaporanController.php
@@ -4,49 +4,24 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\File;
+
 use App\Models\Laporan;
 class LaporanController extends Controller
 {
-
     public function create(){
         return view('create');
     }
-    
-    public function index(){
+
+    public function index()
+    {
         //
         $data = Laporan::latest()->simplepaginate(5);
         $id_latest = Laporan::latest()->first();
-        $id = $id_latest->id;
 
-        return view('Home', compact('data','id'));
+        return view('Home', compact('data'));
     }
 
-    public function store(Request $request){
-        $data = request()->validate([
-            'laporan' => 'required',
-            'aspek' => 'required',
-            'lampiran' => 'required|max:2000|mimes:jpeg,png,jpg,doc,docx,xls,xlsx,ppt,pptx,pdf',
-        ]);
-
-        $id_latest = Laporan::latest()->first();
-        $id_file = $id_latest->id + 1;
-        $nama_file = "lampiran".$id_file;
-        
-        $lampiran = $request->file('lampiran');
-        $lampiran_db = $nama_file.'.'.$lampiran->getClientOriginalExtension();
-        $lampiran->move(public_path('lampiran'), $lampiran_db);
-
-
-        $laporan = new Laporan();
-        $laporan->laporan = request('laporan');
-        $laporan->aspek = request('aspek');
-        $laporan->lampiran = $lampiran_db;
-        $laporan->save();
-        
-
-        return redirect('/');
-    }
-    
     public function detail($id)
     {
         $data = Laporan::find($id);
@@ -77,4 +52,47 @@ class LaporanController extends Controller
         $laporan->save();
         return redirect('/');
     }
+
+    public function store(Request $request){
+        $data = request()->validate([
+            'laporan' => 'required',
+            'aspek' => 'required',
+            'lampiran' => 'required|max:2000|mimes:jpeg,png,jpg,doc,docx,xls,xlsx,ppt,pptx,pdf',
+        ]);
+
+        $id_latest = Laporan::latest()->first();
+        $id_file = $id_latest->id + 1;
+        $nama_file = "lampiran".$id_file;
+        
+        $lampiran = $request->file('lampiran');
+        $lampiran_db = $nama_file.'.'.$lampiran->getClientOriginalExtension();
+        $lampiran->move(public_path('lampiran'), $lampiran_db);
+
+
+        $laporan = new Laporan();
+        $laporan->laporan = request('laporan');
+        $laporan->aspek = request('aspek');
+        $laporan->lampiran = $lampiran_db;
+        $laporan->save();
+        
+
+        return redirect('/');
+    }
+
+    public function destroy($id)
+    {
+        $data = Laporan::find($id);
+        File::delete('lampiran/'.$data->lampiran);
+
+        $data->delete();
+        return redirect('/')->with('Success','Laporan successfuly deleted.');
+    }
+
+    public function search()
+    {
+        $search_input = $_GET['keyword'];
+        $data = Laporan::where('laporan', 'LIKE','%'.$search_input.'%')->orWhere('aspek', 'LIKE','%'.$search_input.'%')->latest()->simplepaginate(5);
+        return view('searchq', compact('data','search_input'));
+    }
+    
 }
