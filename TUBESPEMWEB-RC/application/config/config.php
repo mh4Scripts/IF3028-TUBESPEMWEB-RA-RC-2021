d12309f057f924214006c75b0e6a1589ce95aecb
Romaita <romaita.119140039@student.itera.ac.id>
2021-12-25 14:37:39 +0700

    Penambahan database, controllers

--- TUBESPEMWEB-RC/application/config/config.php
+++ TUBESPEMWEB-RC/application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'localhost\IF3028-TUBESPEMWEB-RA-RC-2021\TUBESPEMWEBRB';
+$config['base_url'] = 'localhost\IF3028-TUBESPEMWEB-RA-RC-2021\TUBESPEMWEB-RC';
 
 /*
 |--------------------------------------------------------------------------
