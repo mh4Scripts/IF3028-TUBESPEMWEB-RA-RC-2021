32c03b8427a49a820e96b7452c7a34823a300b28
andhikapp28 <andhika.119140224@student.itera.ac.id>
2021-12-25 19:18:20 +0700

    update input view and css view

--- resources/views/input.blade.php
+++ resources/views/input.blade.php
@@ -8,11 +8,13 @@
 <div class="container">
     <h2>SIMPLE LAPOR!</h2>
     <div class="main">
-
         <p>Buat Laporan/Komentar</p>
         <hr>
-
         <form action="/prosesdata" method="post" enctype="multipart/form-data" >
+            <div class="judul">
+            <input type="text" name="judul" id="judul" value="Judul Laporan/Komentar">
+            </div>
+
             <textarea name="komentar" id="komentar">Laporan/Komentar</textarea>
 
             <select id="aspek" name="aspek">
@@ -25,11 +27,25 @@
             <input type="file" name="file">
 
             <div class="tombol">
-                <button type="submit">Buat Lapor!</button>
+                <button onclick="validate();" type="submit">Buat Lapor!</button>
             </div>
             <hr>
         </form>
     </div>
 </div>
+
+<script>
+    function validate(){
+        var error = 0,
+        el = document.getElementById("komentar");
+        if(el.value.replace(/\s+/g, '').length <= 20){
+            error++;
+        }
+        if(error > 0) {
+            alert('Komentar Minimal 20 Kata!');
+    }
+}
+</script>
+
 </body>
 </html>
